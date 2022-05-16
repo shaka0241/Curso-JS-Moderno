58cd0d63f3e37445b9023f1042ee63d6e7001a29
Alberto Rojas <alberto.rojas@vasslatam.com>
2022-05-16 13:52:38 -0400

    integrando archivo js y comentarios

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,1 +1,9 @@
-alert("Hola mundo desde archjivo externo");
+//Pregunta al usuario su nombre
+const nombre = prompt('Cual es tu nombre');
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
+
+//Comentario de una linea
+
+/* Comentario
+de varias
+lineas */
